e9de85ac434889af8ca36194a1248dd7be78ad3e
Marlen Temirbaev <116635907+mouflon150@users.noreply.github.com>
2023-09-05 11:51:29 +0600

    Добавьте файлы проекта.

---
+++ LabWork1/LabWork1.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+const double pi = 3.14;
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+
+    // Задача #1
+    /*double d, l, s, r;
+
+    cout << "Введите радиус: ";
+    cin >> r;
+
+    d = 2 * r;
+    l = 2 * pi * r;
+    s = pi * pow(r, 2);
+
+    cout << "Диаметр: " << d << "\nДлина: " << l << "\nПлощадь круга: " << s << endl;
+    return 0;*/
+
+    // Задача #2
+    /*int a, b, c ,s;
+
+    cout << "Введите первый катет: ";
+    cin >> a;
+    cout << "Введите второй катет: ";
+    cin >> b;
+
+    s = (a * b) / 2;
+    c = pow(a, 2) + pow(b, 2);
+    cout << "Площадь треугольника: " << s << "\nГипотенуза: " << c << endl;
+    return 0;*/
+
+
+    // Задача #3
+    //double s, r1, r2;
+
+    //cout << "Внешний радиус: ";
+    //cin >> r2;
+    //r1 = 20; // Внутренний радиус
+    //if (r2 > 20) {
+    //    s = pi * (pow(r2, 2) - pow(r1, 2));
+    //    cout << "Площадь кольца: " << s << endl;
+    //    return 0;
+    //}
+    //else {
+    //    cout << "Где то произошла ошибка!";
+    //    return 1;
+    //}
+
+}
