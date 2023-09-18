d48f26664b573438737b25ab35e34a91672040e6
Marlen Temirbaev <116635907+mouflon150@users.noreply.github.com>
2023-09-18 11:22:31 +0600

    Commit message

---
+++ LabWork2/LabWork2.cpp
@@ -0,0 +1,54 @@
+﻿// LabWork2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+const double pi = 3.14;
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+
+    // Задача #1
+    /*double x, f;
+
+    cout << "Input x: "; 
+    cin >> x;
+
+    if (x <= 0)
+        f = 0;
+    else if (0 < x <= 1)
+        f = pow(x, 2) - x;
+    else
+        f = pow(x, 2) - sin(pi * (x * x));
+    cout << f;
+    return 0;*/
+
+    // Задача #2
+    /*double a, b, c, d, x, x1, x2;
+
+    cout << "Введите действительные числа a, b, c через пробел: ";
+    cin >> a; cin >> b; cin >> c;
+
+    d = b * b - 4 * a * c;
+    if (d > 0) {
+        x1 = (-b + sqrt(d)) / (2 * a);
+        x2 = (-b - sqrt(d)) / (2 * a);
+        cout << "Уравнение имеет два корня: " << endl;
+        cout << "x1 = " << x1 << endl;
+        cout << "x2 = " << x2 << endl;
+    } 
+    else if (d == 0) {
+        x = - b / (2 * a);
+        cout << "x = " << x << endl;
+    }
+    else {
+        cout << "Корней нет";
+    }
+    return 0;*/
+
+    // Задача #3
+
+}
